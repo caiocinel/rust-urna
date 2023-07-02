1b4f436394c446a82765b025f0672f9b081c0c43
Caillou <caioalencarcinel@gmail.com>
2023-07-02 11:56:03 -0300

    Basic votation

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,14 @@ fn main() {
         let mut voted_candidate = String::new();
         io::stdin().read_line(&mut voted_candidate).expect("Failed to read line");
 
-        println!("Input: {}", voted_candidate);
-        println!("Equals 1: {}", voted_candidate.trim() == "1");
-
-        if voted_candidate == String::from("1"){
-            println!("Voted in Candidate 1");
+        if voted_candidate.trim() == "1"{
             candidate_1_votes = candidate_1_votes + 1;
+            println!("Voted in Candidate 1");
         }
-
-        if voted_candidate == String::from("2"){
-            println!("Voted in Candidate 2");
+              
+        if voted_candidate.trim() == "2"{
             candidate_2_votes = candidate_2_votes + 1;
+            println!("Voted in Candidate 2");
         }
 
         if voted_candidate.trim() != ""{
