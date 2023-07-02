cff4792f142baaf249298e34a4382858934a3cac
Caillou <caioalencarcinel@gmail.com>
2023-07-02 12:08:15 -0300

    no show results on vote/add votation ending system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::io;
+use std::{io, process::exit};
+const END_PASSWORD: &str = "123456789";
 
 
 fn main() {    
+
     println!("Starting...");
     let mut candidate_1_votes: i32 = 0;
     let mut candidate_2_votes: i32 = 0;
@@ -10,6 +12,13 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Fail to get input");
         let vote = input.trim();
+
+        if vote == END_PASSWORD{
+            println!("Vote Ended");
+            println!("Candidate 1: {}", candidate_1_votes);
+            println!("Candidate 2: {}", candidate_2_votes);
+            exit(0)
+        }
 
         if vote != "1" && vote != "2"{
             println!("Invalid Vote, try again!");
@@ -24,11 +33,6 @@ fn main() {
         if vote == "2"{
             candidate_2_votes = candidate_2_votes + 1;
             println!("Voted in Candidate 2");
-        }
-
-        if vote != ""{
-            println!("Candidate 1: {}", candidate_1_votes);
-            println!("Candidate 2: {}", candidate_2_votes);
         }
     }
 }
