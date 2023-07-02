eb6e13a501e911b296725eb4e5b2dd1ca3b2d93e
Caillou <caioalencarcinel@gmail.com>
2023-07-02 12:39:22 -0300

    Use namespaces everytime

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
-use std::{io, process::exit};
 const END_PASSWORD: &str = "123456789";
-
 
 fn main() {    
 
     println!("Starting...");
     let mut candidate_1_votes: i32 = 0;
     let mut candidate_2_votes: i32 = 0;
-
-    loop{
+    loop{ 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Fail to get input");
+        std::io::stdin().read_line(&mut input).expect("Fail to get input");
         let vote = input.trim();
 
         if vote == END_PASSWORD{
             println!("Vote Ended");
             println!("Candidate 1: {}", candidate_1_votes);
             println!("Candidate 2: {}", candidate_2_votes);
-            exit(0)
+            std::process::exit(0)
         }
 
         if vote != "1" && vote != "2"{
             println!("Invalid Vote, try again!");
+            std::thread::sleep(std::time::Duration::from_millis(1000));
             continue;
         }
 
@@ -34,5 +32,7 @@ fn main() {
             candidate_2_votes = candidate_2_votes + 1;
             println!("Voted in Candidate 2");
         }
+
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
